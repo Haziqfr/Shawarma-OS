fd2b3eb40dccf5a3db6edb90518bbe099cc4597d
Rafid Islam <goku237411@gmail.com>
2026-08-08 15:22:24 +0600

    refactor(serial): use named UART register offsets

--- include/drivers/serial/serial.h
+++ include/drivers/serial/serial.h
@@ -1,6 +1,30 @@
 #ifndef DRIVERS_SERIAL_SERIAL_H
 #define DRIVERS_SERIAL_SERIAL_H
 
+// Macro Constants
+
+// x86 PC - NS16550
+#define COM1_PORT 0x3F8
+#define THR 0  // Write; Transmitter Holding Register
+#define RHR 0  // Read; Register Holding Register
+#define DLL 0  // Read/Write - DLAB must be set; Divisor Latch Least significant byte
+
+#define IER 1  // Read/Write; Interrupt Enable Register
+#define DLM 1  // Read/Write - DLAB must be set; Divisor Latch Most significant byte
+
+#define ISR 2  // Read; Interrupt Status Register
+#define FCR 2  // Write; FIFO Control Register
+
+#define LCR 3  // Read/Write; Line Control Register
+#define MCR 4  // Read/Write; Modem Control Register
+
+#define LSR 5  // Read; Line Status Register
+#define PSD 5  // Write - DLAB must be set; Prescaler Division
+
+#define MSR 6  // Read; Modem Status Register
+#define SPR 7  // Read/Write; Scratch Pad Register
+
+
 // Function Prototypes
 void serial_init(void);
 void serial_putc(char c);
--- src/kernel/drivers/serial/ns16550.c
+++ src/kernel/drivers/serial/ns16550.c
@@ -1,17 +1,16 @@
-#define COM1_PORT 0x3F8
-
 #include <drivers/serial/serial.h>
 #include <arch/i386/io.h>
 
+
 void serial_init(void)
 {
-	outb(COM1_PORT + 1, 0x00); // Disable all interrupts
-	outb(COM1_PORT + 3, 0x80); // Enable DLAB
-	outb(COM1_PORT + 0, 0x03);
-	outb(COM1_PORT + 1, 0x00);
-	outb(COM1_PORT + 3, 0x03);
-	outb(COM1_PORT + 2, 0xC7);
-	outb(COM1_PORT + 4, 0x03);
+	outb(COM1_PORT + IER, 0x00); // Disable all interrupts
+	outb(COM1_PORT + LCR, 0x80); // Enable DLAB
+	outb(COM1_PORT + DLL, 0x03);
+	outb(COM1_PORT + DLM, 0x00);
+	outb(COM1_PORT + LCR, 0x03);
+	outb(COM1_PORT + FCR, 0xC7);
+	outb(COM1_PORT + MCR, 0x03);
 }
 
 void serial_putc(char c)

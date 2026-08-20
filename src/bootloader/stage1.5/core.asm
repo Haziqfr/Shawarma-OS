a7e03bccb3958404609b91f7b7c0f16f33cca317
Rafid Islam <goku237411@gmail.com>
2026-08-20 23:58:08 +0600

    feat(boot): initialize VBE framebuffer
    - Query VBE for available modes
    - Select a 24+ bpp linear framebuffer mode
    - Store framebuffer information in GenesisPage
    
    NOTE: DO NOT SWITCH VIDEO MODE! LFB support in the kernel is still under
    construction

--- src/bootloader/stage1.5/core.asm
+++ src/bootloader/stage1.5/core.asm
@@ -12,6 +12,9 @@
 BOOTINFO_ADDR      equ 0x6D00
 MMAP_MAX_ENTRIES   equ 128
 MMAP_ENTRY_SIZE    equ 20
+VBE_INFO_ADDR_SEG  equ 0x7000
+VBE_MODEINFO_ADDR_SEG equ 0x7100
+
 MAGIC              equ 0x88FF1A3B ; crc32("ShawarmaOS Boot Protocol")
 
 CODE_SEG equ code_segment_descriptor - gdt_start    ; code segment pointer
@@ -56,6 +59,8 @@ main:
  mov dword [BOOTINFO_ADDR + BootInfo.magic], MAGIC
  mov word [BOOTINFO_ADDR + BootInfo.version], 0x0200
  call get_memory_map
+ call get_vbe_info
+ call get_vbe_mode_info
  mov al, [lba_status]
  cmp al, 2
  je .lba_read
@@ -83,6 +88,8 @@ main:
 
 .stage_finale:
   cli                      ; disable all interrupts
+  push 0
+  pop ds
   lgdt [gdt_descriptor]    ; load gdt table address to GDTR
   mov eax, cr0
   or  eax, 1               
@@ -270,14 +277,158 @@ get_memory_map:
 
 
 .fail:
-    mov si, mm_failed_msg
+    mov si, err_mm_failed_msg
     call puts
+.halt:
     cli
     hlt
-    jmp $-4
+    jmp .halt
 
 .success:
     ret
+
+get_vbe_info:
+    xor ax, ax
+    mov ax, VBE_INFO_ADDR_SEG
+    mov es, ax
+    xor di, di
+
+    mov dword [es:di + VbeInfo.signature], 'VBE2'
+
+    ; ah = 4Fh - VBE function, al = 00h - Get SuperVGA Info
+    mov ax, 0x4F00
+    int 0x10
+
+    cmp ax, 0x004F
+    jne .fail
+
+    ret
+
+.fail:
+    mov si, err_framebuff_failed_msg
+    call puts
+.halt:
+    cli
+    hlt
+    jmp .halt
+
+;
+; Preferably we want the highest resolution and 32bit Bit Per Pixel
+; with Linear Frame Buffer
+;
+
+get_vbe_mode_info:
+    push VBE_INFO_ADDR_SEG
+    pop ds
+    lds si, [VbeInfo.video_mode_ptr]
+
+    ; Load the SuperVGA mode info buffer at es:di
+    push VBE_MODEINFO_ADDR_SEG
+    pop es
+    xor di, di
+
+    cld ; ensure DF = 0 so LODSW increments SI
+
+.next_mode:
+    lodsw ; Loads the mode number from ds:si to AX then increments si by 2
+    cmp ax, 0xFFFF
+    je .done
+
+    push si
+    push ax ; preserve mode number
+
+    xchg ax, cx ; exchange the contents of AX to CX and vice versa
+    mov ax, 0x4F01
+    int 0x10
+
+    ;jc .skip_mode
+    cmp ax, 0x004F
+    jne .skip_mode
+
+    mov ax, [es:ModeInfo.mode_attrib]
+    and ax, 0x0090 ; we only care about bit4 (graphics mode) and bit7 (lfb support)
+    cmp ax, 0x0090
+    jne .skip_mode
+
+    mov bl, byte [es:ModeInfo.depth]
+    cmp bl, 24 ; only support 24+ bpp/depth
+    jb .skip_mode
+
+    movzx eax, word [es:ModeInfo.x_resolution]
+    movzx ecx, word [es:ModeInfo.y_resolution]
+    mul ecx
+
+    movzx ecx, bl
+    imul ecx, ecx, 10000000
+    add eax, ecx
+
+    cmp eax, [cs:best_vbe_mode_score]
+    jbe .skip_mode
+
+    mov dword [cs:best_vbe_mode_score], eax
+    pop ax
+    push ax
+    mov word [cs:best_vbe_mode], ax
+
+.skip_mode:
+    pop ax
+    pop si
+    jmp .next_mode
+
+.done:
+    cmp dword [cs:best_vbe_mode_score], 0
+    je .fail
+
+    ; load the best mode buff
+    push VBE_MODEINFO_ADDR_SEG
+    pop es
+    xor di, di
+    mov cx, word [cs:best_vbe_mode]
+    mov ax, 0x4F01
+    int 0x10
+
+.finale:
+    push 0
+    pop gs
+
+    mov eax, dword [es:ModeInfo.phys_base_ptr]
+    mov dword [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.lfb_base], eax
+
+    mov eax, dword [es:ModeInfo.x_resolution]
+    mov dword [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.lfb_width], eax
+
+    mov ax, [es:ModeInfo.bytes_per_scan_line]
+    mov word [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.lfb_pitch], ax
+
+    mov al, [es:ModeInfo.depth]
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.lfb_depth], al
+
+    mov ax, word [es:ModeInfo.red_size]
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.red_pos], ah
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.red_size], al
+
+    mov ax, word [es:ModeInfo.green_size]
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.green_pos], ah
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.green_size], al
+
+    mov ax, word [es:ModeInfo.blue_size]
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.blue_pos], ah
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.blue_size], al
+
+    mov ax, word [es:ModeInfo.reserved_size]
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.reserved_pos], ah
+    mov byte [gs:BOOTINFO_ADDR + BootInfo.screen_info + screen_info.reserved_size], al
+
+    ret
+
+.fail:
+    mov si, err_mode_failed_msg
+    call puts
+.halt:
+    cli
+    hlt
+    jmp .halt
+
 
 puts:
  push si
@@ -345,6 +496,9 @@ DAP:
 boot_drive: db 0
 lba_status: db 0
 
+best_vbe_mode: dw 0
+best_vbe_mode_score: dd 0
+
 struc screen_info
     .lfb_base:   resb 8  ; 0x00
     .lfb_size:   resb 8  ; 0x08
@@ -384,13 +538,99 @@ struc BootInfo
 
     .reserved1:     resb 1464  ; 0xA48
 
+endstruc
 
 %assign BootInfoSize BootInfo_size
 static_assert (BootInfoSize == 4096), CRITICAL: BootInfo must be 4096 bytes. Current size: %[BootInfoSize] bytes
+
+struc VbeInfo
+    .signature:            resb 4   ; 0x000
+    .version:              resb 2   ; 0x004
+    .oem_string_ptr:       resb 4   ; 0x006
+    .capabilities:         resb 4   ; 0x00A
+    .video_mode_ptr:       resb 4   ; 0x00E
+    .total_mem:            resb 2   ; 0x012
+
+    ; OEM metadata - irrelevant to us for now
+    .oem_sftwr_rev:        resb 2   ; 0x014
+    .oem_vendor_name_ptr:  resb 4   ; 0x016
+    .oem_product_name_ptr: resb 4   ; 0x01A
+    .oem_product_rev_ptr:  resb 4   ; 0x01E
+    .reserved:             resb 222 ; 0x022
+    .oem_data:             resb 256 ; 0x100
 endstruc
 
+%assign VbeInfoSize VbeInfo_size
+static_assert (VbeInfoSize == 512), CRITICAL: VbeInfo must be 512 bytes. Current size: %[VbeInfoSize] bytes
+
+struc ModeInfo
+    ; Available for all VBE rev
+    .mode_attrib:     resb 2       ; 0x00
+    .winA_atrib:     resb 1       ; 0x02
+    .winB_atrib:     resb 1       ; 0x03
+    .win_granuality: resb 2       ; 0x04
+    .win_size:       resb 2       ; 0x06
+    .winA_seg:       resb 2       ; 0x08
+    .winB_seg:       resb 2       ; 0x0A
+    .win_func_ptr:   resb 4       ; 0x0C
+    .bytes_per_scan_line: resb 2  ; 0x10
+
+    ; Available for VBE 1.2+
+    .x_resolution: resb 2         ; 0x12
+    .y_resolution: resb 2         ; 0x14
+    .x_char_size:  resb 1         ; 0x16
+    .y_char_size:  resb 1         ; 0x17
+    .mem_plane_count: resb 1      ; 0x18
+    .depth:        resb 1         ; 0x19
+    .bank_count:   resb 1         ; 0x1A
+    .mem_model:    resb 1         ; 0x1B
+    .bank_size:    resb 1         ; 0x1C
+    .img_page_count: resb 1       ; 0x1D
+    ._reserved0:   resb 1         ; 0x1E
+
+    ; Direct Color fields
+    .red_size:   resb 1           ; 0x1F
+    .red_pos:    resb 1           ; 0x20
+    .green_size: resb 1           ; 0x21
+    .green_pos:  resb 1           ; 0x22
+    .blue_size:  resb 1           ; 0x23
+    .blue_pos:   resb 1           ; 0x24
+    .reserved_size: resb 1        ; 0x25
+    .reserved_pos: resb 1         ; 0x26
+    .direct_color_mode_info: resb 1 ; 0x27
+
+    ; Available for VBE 2.0+
+    .phys_base_ptr: resb 4        ; 0x28
+    ._reserved1:    resb 6        ; 0x2C
+
+    ; Available for VBE 3.0+
+    .lin_bytes_scan_line: resb 2  ; 0x32
+    .bank_img_page_count: resb 1  ; 0x34
+    .lin_img_page_count:  resb 1  ; 0x35
+    .lin_red_size:        resb 1  ; 0x36
+    .lin_red_pos:         resb 1  ; 0x37
+    .lin_green_size:      resb 1  ; 0x38
+    .lin_green_pos:       resb 1  ; 0x39
+    .lin_blue_size:       resb 1  ; 0x3A
+    .lin_blue_pos:        resb 1  ; 0x3B
+    .lin_rsvd_size:       resb 1  ; 0x3C
+    .lin_rsvd_pos:        resb 1  ; 0x3D
+    .max_pixel_clock:     resb 4  ; 0x3E
+
+    ._reserved2: resb 190         ; 0x42
+
+endstruc
+
+%assign ModeInfoSize ModeInfo_size
+static_assert (ModeInfoSize == 256), \
+    CRITICAL: ModeInfo must be 256 bytes. Current size: %[ModeInfoSize] bytes
+
+; Messages
 msg: db "I am stage1.5, I am alive", 0x0D, 0x0A, 0
 
-mm_failed_msg: db "Unable to detect Memory Map.", 0x0D, 0x0A, "Halting...", 0x0D, 0x0A, 0
+; Error messages
+err_mm_failed_msg: db "Unable to detect Memory Map", 0x0D, 0x0A, "Halting...", 0x0D, 0x0A, 0
+err_framebuff_failed_msg: db "Unable to get framebuffer", 0x0D, 0x0A, "Halting...", 0x0D, 0x0A, 0
+err_mode_failed_msg: db "Unable to get suitable VBE mode", 0x0D, 0x0A, "Halting...", 0x0D, 0x0A, 0
 
 times 1024-($-$$) db 0

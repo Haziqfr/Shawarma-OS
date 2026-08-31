06b6c41dfad99e2547d7425f5fcc344cff772a2e
Rafid Islam <goku237411@gmail.com>
2026-08-31 20:09:10 +0600

    feat(kernel): reload GDT at kernel entry

--- src/kernel/entry.asm
+++ src/kernel/entry.asm
@@ -5,8 +5,13 @@ extern kernel_main
 global _start
 
 _start:
+    ; Reload GDT
+    lgdt [gdt_descriptor]
+    jmp 0x08:.reload_cs     ; Far jump to reload CS
+
+.reload_cs:
     ; setting up segment registers
-    mov ax, 0x10
+    mov ax, 0x10        ; 0x10 = Data Segment
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -20,15 +25,36 @@ _start:
     call kernel_main
     add esp, 4
 
-
-    jmp .halt
-
-
-
 .halt:
     cli
     hlt
     jmp .halt    ; loop forever
 
 
+gdt_start:
 
+null_descriptor:
+    dd 0
+    dd 0
+
+code_segment_descriptor:
+    dw 0xFFFF        ; Limit (0-15)
+    dw 0x0000        ; Base (0-15)
+    db 0x00          ; Base (16-23)
+    db 10011010b     ; Access Byte (0-7): P=1, DPL=00, S=1, E=1, DC=0(Non-Conforming), RW=1(Read true), A=0
+    db 11001111b     ; Flags (0-3)(upper 4 bit) G=1, DB=1, L=not applicable, AVL=0(IDK why) | Limit (16-19) (lower 4 bit)
+    db 0x00          ; Base (24-31)
+
+data_segment_descriptor:
+    dw 0xFFFF        ; Limit (0-15)
+    dw 0x0000        ; Base (0-15)
+    db 0x00          ; Base (16-23)
+    db 10010010b     ; Access Byte (0-7): P=1, DPL=00, S=1, E=0, DC=0(Direction Up), RW=1(Write true), A=0
+    db 11001111b     ; Flags (0-3)(upper 4 bit): G=1, DB=1, L=not applicable, AVL=0(IDK why) | Limit (16-19) (lower 4 bit)
+    db 0x00          ; Base (24-31)
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start -1
+    dd gdt_start
